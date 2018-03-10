d787e25448bda08c00839a4cdcc5e4d1545ffafa
Abhimanyue Singh Tanwar <abc@example.com>
2018-03-11 04:51:55 +0800

    Add number of days left

--- notification.js
+++ notification.js
@@ -3,28 +3,36 @@ window.onload = function(){
 
    chrome.storage.sync.get(null,function(assignments){
       var s="";
+      var diffdays = 0;
+      var today = new Date();
+      var oneDay = 24*60*60*1000;
+
       for (key in assignments)
       {
         var as=assignments[key];
         if(key!="counter")
           {s= as.courseName+" "+as.deadline+" "+as.status+"\n";
 
+
           //to sort the assignment according to submission status
           if(as.status!="Submitted")
             {var id = guidGenerator();
+            var deadlinedate = new Date(as.deadline);
+            diffdays = Math.round(Math.abs((deadlinedate.getTime() - today.getTime())/(oneDay)));
 
-            var html = "<li class='card coursecard'" + "id=" + "'" + id + "'" + ">" + "<span id='cardtext'>" + "<span id='cardtitle'>" +  as.courseName + "</span>" + "<br><span id='cardBody'>" + "Due on:" + "  " +as.deadline + "</span>"+"</span>" +"</li>";
+            var html = "<li class='card coursecard'" + "id=" + "'" + id + "'" + ">" + "<span class='cardtext'>" + "<span class='cardtitle'>" +  as.courseName + "</span>" + "<br><span class='cardBody'>" + "Due on:" + "  " + as.deadline+ "</span>"+ "<br><span class='cardDays'>" + diffdays + "</span>" + "<span class = 'cardtitle'>" + "   "  + " Days Left" + "</span>" +   "</li>";
             document.getElementById("duecardlist").innerHTML+= html;
 
             var colors = ['#7b1fa2', '#e53935', '#c2185b','#0d47a1', '#512da8', '#004d40' , '#2e7d32' , '#1b5e20'];
             var random_color = colors[Math.floor(Math.random() * colors.length)];
             document.getElementById(id).style.backgroundColor = random_color;
+           
             }
 
           else
           {
             var id = guidGenerator();
-            var html = "<li class='card coursecard'" + "id=" + "'" + id + "'" + ">" + "<span id='cardtext'>" + "<span id='cardtitle'>" +  as.courseName + "</span>" + "<br><span id='cardBody'>" + "Submitted on:" + "  " +as.deadline + "</span>"+"</span>" +"</li>";
+            var html = "<li class='card coursecard'" + "id=" + "'" + id + "'" + ">" + "<span class='cardtext'>" + "<span class='cardtitle'>" +  as.courseName + "</span>" + "<br><span class='cardBody'>" + "Completed on:" + "  " +as.submittedOn + "</span>"+"</span>" +"</li>";
             document.getElementById("submittedcardlist").innerHTML+= html;
 
             var colors = ['#7b1fa2', '#e53935', '#c2185b','#0d47a1', '#512da8', '#004d40' , '#2e7d32' , '#1b5e20'];
