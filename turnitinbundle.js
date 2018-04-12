857072f4fe64ce312d7262a8276247b10861c694
Sudhakar Shah <sudhakarshah2013@gmail.com>
2018-04-12 19:12:52 +0800

    added complete date for turnitin assignments

--- turnitinbundle.js
+++ turnitinbundle.js
@@ -12,14 +12,22 @@ if(document.getElementsByClassName('left cell c2')[0].innerText=="--")
     status="Not Submitted";
 }
 
+if(status="Submitted")
+{
+  submittedOn=document.getElementsByClassName('right cell c7')[0].innerText;
+}
+var slashIndex1=submittedOn.indexOf('/');
+var slashIndex2=submittedOn.indexOf('/',slashIndex1+1);
+submittedOn=submittedOn.substr(slashIndex1+1,slashIndex2-slashIndex1-1)+'/'+submittedOn.substr(0,slashIndex1)+'/'+submittedOn.substr(slashIndex2+1);
+
 //finding course name
 var aTag=document.querySelectorAll('[itemprop="url"]')[1];
 courseName=aTag.getAttribute("title");
-console.log(courseName);
+
 
 // finding assignment name
 assignmentName=document.querySelectorAll('[itemprop="title"]')[3].innerText;
-console.log(assignmentName);
+
 
 if (deadline!="Undefined"){
   update(courseName,assignmentName,deadline,status,submittedOn);
